2c92523fd734bceae32da5b1fa8950e677166c49
Xin Zhe <xinzhe0127@gmail.com>
2021-12-29 17:12:38 +0800

    add base service

---
+++ src/main/java/com/example/demo/service/impl/BaseServiceImpl.java
@@ -0,0 +1,76 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.service.BaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public abstract class BaseServiceImpl<T, ID> implements BaseService<T, ID> {
+
+    /**
+     * Find respective DAO
+     *
+     * @return
+     */
+    @SuppressWarnings("none")
+    @Autowired
+    protected abstract JpaRepository getRepository();
+
+    @Override
+    public <S extends T> S save(S entity) {
+        return (S) getRepository().save(entity);
+    }
+
+    @Override
+    public <S extends T> Iterable<S> saveAll(Iterable<S> entities) {
+        return getRepository().saveAll(entities);
+    }
+
+    @Override
+    public Optional<T> findById(ID id) {
+        return getRepository().findById(id);
+    }
+
+    @Override
+    public boolean existsById(ID id) {
+        return getRepository().existsById(id);
+    }
+
+    @Override
+    public Iterable<T> findAll() {
+        return getRepository().findAll();
+    }
+
+    @Override
+    public Iterable<T> findAllById(Iterable<ID> var1) {
+        return getRepository().findAll();
+    }
+
+    @Override
+    public Long count() {
+        return getRepository().count();
+    }
+
+    @Override
+    public void deleteById(ID id) {
+        getRepository().deleteById(id);
+    }
+
+    @Override
+    public void delete(T entity) {
+        getRepository().delete(entity);
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends T> allID) {
+        getRepository().deleteAll(allID);
+    }
+
+    @Override
+    public Page<T> findAll(Pageable pageable) {
+        return getRepository().findAll(pageable);
+    }
+}
