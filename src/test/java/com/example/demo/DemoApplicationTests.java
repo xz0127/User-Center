12e86e18afee55b7e295a11298fbc1999feecb4a
Xin Zhe <xinzhe0127@gmail.com>
2022-01-05 22:03:23 +0800

    add basic test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import com.example.demo.common.utils.JwtTokenUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +9,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 class DemoApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void testToken() throws Exception {
+        String userId = "test123";
+        String username = "123456";
+        String token = JwtTokenUtils.createJWT(userId, username);
+        Assertions.assertFalse(JwtTokenUtils.isExpiration(token));
+        Assertions.assertEquals(username, JwtTokenUtils.getUsername(token));
+        Assertions.assertEquals(userId, JwtTokenUtils.getUserId(token));
     }
 
 }
