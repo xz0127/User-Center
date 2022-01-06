9e6a5aa8c69046b80215841b8a82f158424debea
Xin Zhe <xinzhe0127@gmail.com>
2022-01-06 18:09:53 +0800

    add support for cross origin

--- src/main/java/com/example/demo/common/config/WebConfig.java
+++ src/main/java/com/example/demo/common/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.example.demo.common.config;
 
 import com.example.demo.common.interceptor.JwtInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +12,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new JwtInterceptor());
+    }
+
+    // support cross origin
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "DELETE", "PUT", "PATCH", "OPTIONS", "HEAD")
+                .maxAge(3600 * 24);
     }
 }
